e1f36b4faef5dbb5d7590006b73960f412e90e91
virgilebouali <virgile.boualigmail.com>
2023-06-13 14:03:39 +0200

    WeatherApp

---
+++ app.js
@@ -0,0 +1,54 @@
+const apiKey = "3eaa689a782df0b4abf4ac13f0216270";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status == 404){
+
+        document.querySelector('.error').style.display = "block";
+        document.querySelector('.weather').style.display = "none";
+    }
+    else {
+        let data = await response.json();
+
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/images/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/images/snow.png";
+    }
+    else if (data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/images/mist.png";
+    }
+
+    document.querySelector('.weather').style.display = "block";
+    document.querySelector('.error').style.display = "none";
+
+    }
+    
+}
+
+
+searchBtn.addEventListener("click", ()=> {
+checkWeather(searchBox.value);
+})
+
+checkWeather();
